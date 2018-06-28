dc523e501e14603beba4f0cfb6755e96dd1714d8
andyliang <2825523288@qq.com>
2018-06-28 17:52:58 +0800

    config service

---
+++ src/test/java/hx/common/cloud/config/AppTest.java
@@ -0,0 +1,20 @@
+package hx.common.cloud.config;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
